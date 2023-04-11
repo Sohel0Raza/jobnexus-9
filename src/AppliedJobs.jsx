f93c85dd8d56a0331d258f41b3063aebb7df5fed
Masud Rana <si05@gtrbd.net>
2023-04-11 14:47:53 +0600

    filter remote and onsite jobs

--- src/AppliedJobs.jsx
+++ src/AppliedJobs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getJob } from '../public/dataBase';
 import { useLoaderData } from 'react-router-dom';
 import JobsApply from './JobsApply';
@@ -10,20 +10,31 @@ const AppliedJobs = () => {
     const saveJobs = getJob()
     for(const id in saveJobs){
         const foundJob = allJobs.find(job => job.id == id)
-        console.log('foundJob :', foundJob);
         if(foundJob){
             jobs.push(foundJob)
         }
     }
-    console.log('jobs :', jobs);
+    const [allJob, setAllJob] = useState(jobs)
+    const handleRemote = () =>{
+       const job = allJob.filter(job => job.job_type == "Remote")
+        setAllJob(job)
+    }
+    const handleOnsite = () =>{
+       const job = allJob.filter(job => job.job_type == "Onsite")
+        setAllJob(job)
+    }
     return (
         <div>
             <div className='bg-secondary mb-36 py-16'>
                 <h2 className='text-2xl font-bold text-center text-primary'>Applied Jobs</h2>
             </div>
+            <div className='text-end w-10/12'>
+            <button onClick={handleRemote} className='btn bg-gradient-to-r from-primary to-sky-500 py-1 text-white mt-3 mr-5 hover:bg-secondary border-none'>Remote</button>
+            <button onClick={handleOnsite} className='btn bg-gradient-to-r from-primary to-sky-500 py-1 text-white mt-3 hover:bg-secondary border-none'>Onsite</button>
+            </div>
             <div>
                 {
-                    jobs.map(job=><JobsApply 
+                    allJob.map(job=><JobsApply 
                     key={job.id}
                     job={job}
                     ></JobsApply>)
